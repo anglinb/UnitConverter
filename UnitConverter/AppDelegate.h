c7be8cad3e567469d9256b5aa7a1c3a0a1993c4b
Brian <laxnski@gmail.com>
2014-02-18 12:12:03 -0800

    Initial Commit

---
+++ UnitConverter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UnitConverter
+//
+//  Created by Brian Anglin on 2/18/14.
+//  Copyright (c) 2014 Brian Anglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
